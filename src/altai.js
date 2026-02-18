bb21f05b26310e4e26d0c80df33f677fec01dc75
Sebastian Clavijo Suero (Personal) <128645385+sclavijosuero@users.noreply.github.com>
2026-02-17 21:02:49 -0800

    TODO list

--- src/altai.js
+++ src/altai.js
@@ -1,3 +1,8 @@
 export * from './googleai/core.js';
 export * from './groqai/core.js';
 export * from './openai/core.js';
+
+// TODO:
+// - Add more models
+// - Remove for OpenAI the option dangerouslyAllowBrowser: true (move node task -still need to be test tool independent-)
+// - Know more about sending files (insteads of 'url' or 'base64')
